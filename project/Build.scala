08281f4d8553ec27ec843aa07d41a78eb11844dd
Rick Snyder <rsnyder@anypresence.com>
2012-09-11 16:57:19 -0400

    A few changes after moving the boot strap out into it's own plugin

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,12 @@ object ApplicationBuild extends Build {
 
 
     val appDependencies = Seq(
-      "org.apache.httpcomponents" % "httpclient" % "4.1.1"
+      "org.apache.httpcomponents" % "httpclient" % "4.1.1",
+      "com.anypresence.play" % "morphia-bootstrap_2.9.1" % "1.0-SNAPSHOT",
+      "com.google.code.morphia"    % "morphia"               % "1.00-SNAPSHOT",
+      //"com.google.code.morphia"    % "morphia"               % "0.99",
+      "com.google.code.morphia"    % "morphia-logging-slf4j" % "0.99",
+      "com.google.code.morphia"    % "morphia-validation"    % "0.99" 
       //"net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.0.0" 
       //"com.google.code" % "morphia" % "0.99" from "http://morphia.googlecode.com/svn/mavenrepo/",
       //"cglib" % "cglib-nodep" % "2.1_3",
@@ -18,6 +23,7 @@ object ApplicationBuild extends Build {
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
+      resolvers ++= Seq("Morphia" at "http://morphia.googlecode.com/svn/mavenrepo/"): _*
     )
 
 }
