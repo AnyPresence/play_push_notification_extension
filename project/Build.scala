720b2759bc992fe8cf289885ce42bc33aa3cac07
Rick Snyder <rsnyder@anypresence.com>
2012-10-16 20:13:31 -0400

    Clean up some code, remove comments

--- project/Build.scala
+++ project/Build.scala
@@ -14,37 +14,21 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       "com.anypresence.play" % "morphia-bootstrap_2.9.1" % "1.0-SNAPSHOT",
       "com.google.code.morphia"    % "morphia"               % "1.00-SNAPSHOT",
-      //"com.google.code.morphia"    % "morphia"               % "0.99",
       "com.google.code.morphia"    % "morphia-logging-slf4j" % "0.99",
       "com.google.code.morphia"    % "morphia-validation"    % "0.99" 
-      //"net.vz.mongodb.jackson" %% "play-mongo-jackson-mapper" % "1.0.0" 
-      //"com.google.code" % "morphia" % "0.99" from "http://morphia.googlecode.com/svn/mavenrepo/",
-      //"cglib" % "cglib-nodep" % "2.1_3",
-      //"com.thoughtworks.proxytoys" % "proxytoys" % "1.0" from "http://morphia.googlecode.com/svn/mavenrepo/"
     )
-
-    def recursiveListFiles(f: File): Seq[File] = {
-      val these = f.listFiles
-      these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
-    }
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
       resolvers ++= Seq("Morphia" at "http://morphia.googlecode.com/svn/mavenrepo/"): _*).settings(
-
-	//sourceGenerators in Compile := Seq()
-        /*sourceGenerators in Test <+= sourceManaged in Test map { dir =>
-          def compiler = Router.RoutesCompiler
-          compiler.compile(new File("/Users/rsnyder/git/play-push-notification-extension/test/routes"), dir, List() )
-          def list = recursiveListFiles(dir)
-          list.filter(_.isFile)
-         }*/
-        ).settings(
-        //unmanagedResources in Test := List()
+        // Ensure that the application.conf and routes files are not included in the compiled jar file
+        // when it is packaged
 	defaultExcludes in Compile in unmanagedResources := "*.conf" || "routes" || "Route*"
       ).settings(
+        // Ensure that the application.conf and routes files ARE included for testing
         defaultExcludes in Test in unmanagedResources := ""
       ).settings(
+          // Exclude all compiled routing classes when the application is packaged
           mappings in (Compile,packageBin) ~= { (ms: Seq[(File, String)]) =>
             val routesStraightFilter = """(Routes\$?)(\$anon.*)?.class""".r
             val controllerRoutesFilter = """([\w\$/]*)controllers/routes([\w\$]*).class""".r
--- test/controller/push/PubSubControllerTest.java
+++ test/controller/push/PubSubControllerTest.java
@@ -18,36 +18,17 @@ public class PubSubControllerTest {
 
 	  @Test
 	  public void testSubscribe() {
-		  //TestServer ts = testServer(PORT);
-		  //start(ts);
-		  
-		  //Result result = callAction(controllers.push.routes.ref.PubSubController.subscribe());
-		  
-		  //WS.Response resp = WS.url(TEST_SERVER_URL + "/push/subscribe").post("{ \"channel\" : \"my_channel\" , \"deviceToken\" : \"AAA\" }").get().;
 		  
 		  TestServer ts = testServer(PORT);
 		  
 		  running(ts, new Runnable() {
 		      public void run() {
-		    		 /*MorphiaBootstrapPlugin p = Play.application().plugin(MorphiaBootstrapPlugin.class);
-		    		 System.out.println("mongodb.uri : " + Play.application().configuration().getString("mongodb.uri"));
-		    		 System.out.println("p : " + p);
-		    		 FakeRequest req =  fakeRequest(POST, "/push/subscribe");
-		    		 req = req.withHeader("Content-Type", "application/json").withHeader("User-Agent", "iPhone").withJsonBody(Json.parse("{ \"channel\" : \"my_channel\", \"deviceToken\" : \"AAA\" }"));
-		    		 System.out.println(routeAndCall(req));*/
-		    		 
-		    		 
-		    		 
-		    		 
-		    		 // Result result = callAction(controllers.push.routes.ref.PubSubController.subscribe());
-		    	  //System.out.println(result);
 		         assertThat(
 		           WS.url(TEST_SERVER_URL + "/push/subscribe")
 		             .setHeader("User-Agent", "iPhone")
 		             .setHeader("Content-Type", "application/json")
-		           	 .post("{ \"channel\" : \"my_channel\" , \"deviceToken\" : \"AAA\" }").get().getStatus()
-		    	  //System.out.println(WS.url(TEST_SERVER_URL + "/push/subscribe").post("{ \"channel\" : \"my_channel\" , \"deviceToken\" : \"AAA\" }").get().getBody());
-		         ).isEqualTo(OK);
+		          	 .post("{ \"channel\" : \"my_channel\" , \"deviceToken\" : \"AAA\" }").get().getStatus()
+			).isEqualTo(OK);
 		      }
 		  });
 
