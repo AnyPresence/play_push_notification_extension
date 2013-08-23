5dc4de2f15f06bc75a80a61a58791faca3da5886
Rick Snyder <rsnyder@anypresence.com>
2013-08-23 10:38:55 -0400

    Add production boolean value to constructor

--- app/workers/LifecycleTriggeredPushNotification.scala
+++ app/workers/LifecycleTriggeredPushNotification.scala
@@ -2,12 +2,13 @@ package workers
 
 import play.api.Logger._
 
-class LifecycleTriggeredPushNotification(val channel: String, val badge: String, val alert: String, val sound : String, val messagePayload: String) extends Runnable
+class LifecycleTriggeredPushNotification(val prod: Boolean, val channel: String, val badge: String, val alert: String, val sound : String, val messagePayload: String) extends Runnable
 {
   
   def run: Unit = {
     // STUB
     debug("LifecycleTriggeredPushNotification is running")
+    debug("  prod           : " + prod)
     debug("  channel        : " + channel)
     debug("  badge          : " + badge)
     debug("  alert          : " + alert)
