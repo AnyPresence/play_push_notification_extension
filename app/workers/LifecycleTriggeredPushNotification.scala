596f277c0eb91240def156f1eef85c79daaa4c82
Rick Snyder <rsnyder@anypresence.com>
2013-08-23 10:33:27 -0400

    Add constructor and debug statements

--- app/workers/LifecycleTriggeredPushNotification.scala
+++ app/workers/LifecycleTriggeredPushNotification.scala
@@ -1,10 +1,18 @@
 package workers
 
-class LifecycleTriggeredPushNotification extends Runnable
+import play.api.Logger._
+
+class LifecycleTriggeredPushNotification(val channel: String, val badge: String, val alert: String, val sound : String, val messagePayload: String) extends Runnable
 {
   
   def run: Unit = {
     // STUB
+    debug("LifecycleTriggeredPushNotification is running")
+    debug("  channel        : " + channel)
+    debug("  badge          : " + badge)
+    debug("  alert          : " + alert)
+    debug("  sound          : " + sound)
+    debug("  messagePayload : " + messagePayload)            
   }
   
 }
